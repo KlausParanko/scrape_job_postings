5899a968dad368bc14fe2a81bb58d9982adeda02
KlausParanko <kparanko@gmail.com>
2023-11-01 14:42:47 +0200

    fixed DATA_FOLDER.joinpath() calls. Nested directories used / as separater when they needed to be a list of arguments

--- data_paths.py
+++ data_paths.py
@@ -5,25 +5,24 @@ DATA_FOLDER = Path("./data/")
 DATA_FOLDER.mkdir(exist_ok=True)
 
 POSTINGS_LIST_PATHS = {
-    # contains results of each time this script is run
-    "RAW_FOLDER": DATA_FOLDER.joinpath("/postings_list/raw"),
+    "RAW_FOLDER": DATA_FOLDER.joinpath("postings_list", "raw"),
     # contains deduplicated and merged results
-    "MERGED": DATA_FOLDER.joinpath("/postings_list/merged.csv"),
+    "MERGED": DATA_FOLDER.joinpath("postings_list", "merged.csv"),
     # MERGED but on azure blob storage
     "AZURE_FILENAME": "postings_list.csv",
 }
 POSTINGS_LIST_PATHS["RAW_FOLDER"].mkdir(exist_ok=True)
 
 POSTINGS_CONTENT_PATHS = {
-    "RAW_FOLDER": DATA_FOLDER.joinpath("/postings/raw"),
-    "MERGED": DATA_FOLDER.joinpath("/postings/merged.parquet"),
+    "RAW_FOLDER": DATA_FOLDER.joinpath("postings", "raw"),
+    "MERGED": DATA_FOLDER.joinpath("postings", "merged.parquet"),
     "AZURE_FILENAME": "postings.parquet",
 }
 
 PARSED_POSTINGS_PATH = {
-    "WHOLE_POSTING": DATA_FOLDER.joinpath("/parsed_postings/parsed_postings.parquet"),
+    "WHOLE_POSTING": DATA_FOLDER.joinpath("parsed_postings", "parsed_postings.parquet"),
     "BULLET_LISTS": DATA_FOLDER.joinpath(
-        "/parsed_postings/posting_bullet_lists.parquet"
+        "parsed_postings", "posting_bullet_lists.parquet"
     ),
     "AZURE_FILENAME": "parsed_postings.parquet",
 }
